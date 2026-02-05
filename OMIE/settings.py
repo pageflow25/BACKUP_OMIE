62e3948bd578a7b4eb0ed4cba56e6cd931ae40fd
Arthur <arthur@gmail.com>
2026-02-05 15:36:42 -0300

    feat: add static files configuration and update URL patterns for serving static files in development

--- OMIE/settings.py
+++ OMIE/settings.py
@@ -160,6 +160,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'staticfiles']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- OMIE/urls.py
+++ OMIE/urls.py
@@ -14,7 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
 from django.views.generic import RedirectView
 from core.views import select_database
@@ -24,3 +27,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('select-database/', select_database, name='select_database'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
